b7ed322852ce14ff6099427bb0f498aa00e232f9
Girish kudachi09 <95070083+GRKUDACHI@users.noreply.github.com>
2025-06-11 10:17:37 +0530

    Delete travels directory

--- travels/src/main/java/net/travels/travels/Repository/TravelRepo.java
+++
@@ -1,9 +0,0 @@
-package net.travels.travels.Repository;
-
-import net.travels.travels.entity.TravelHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TravelRepo extends JpaRepository<TravelHistory,Long>
-{
-
-}
