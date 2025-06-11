cdbbceee7fc2339d5c225e328a073b7176ba4067
Girish kudachi09 <95070083+GRKUDACHI@users.noreply.github.com>
2025-06-11 10:20:32 +0530

    Delete journalApp directory

--- journalApp/src/main/java/net/journal/journalApp/repository/JournalEntryRepository.java
+++
@@ -1,9 +0,0 @@
-package net.journal.journalApp.repository;
-
-import net.journal.journalApp.entity.journalEntry;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JournalEntryRepository extends JpaRepository<journalEntry, Long>
-{
-
-}
--- journalApp/src/main/java/net/journal/journalApp/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package net.journal.journalApp.repository;
-
-import net.journal.journalApp.entity.user;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<user, Long> {
-}
